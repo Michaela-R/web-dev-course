3427e0457ccc92af988274419de1f1e50384f11c
Marian-N <mariannisky@gmail.com>
2022-06-12 18:58:05 +0200

    Secrets - connect to database with mongoose

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,8 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 3000;
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/?retryWrites=true&w=majority`);
+
+const userSchema = {
+    email: String,
+    password: String
+};
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -12,13 +26,26 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.get("/register", (req, res) => {
-    res.render("register");
-});
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        console.log(req.body)
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.render("secrets");
+            }
+        });
+    });
 
-app.set("view engine", "ejs");
-app.set(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT);
 });
