20d1c111c40cbaebc18036c7dd1c7ff5cf6bd528
Marian-N <mariannisky@gmail.com>
2022-06-12 20:04:45 +0200

    secrets - add mongoose-encryption

--- Secrets/app.js
+++ Secrets/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import encrypt from "mongoose-encryption";
 dotenv.config();
 
 const PORT = 3000;
@@ -12,26 +13,39 @@ app.use(express.static("public"));
 
 mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}/?retryWrites=true&w=majority`);
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-};
+});
+userSchema.plugin(encrypt, {secret: process.env.DATABASE_SECRET, encryptedFields: ["password"]});
+
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/login", (req, res) => {
-    res.render("login");
-});
+app.route("/login")
+    .get((req, res) => {
+        res.render("login");
+    })
+    .post((req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username}, (err, foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password)
+                    res.render("secrets");
+            }
+        });
+    });
 
 app.route("/register")
     .get((req, res) => {
         res.render("register");
     })
     .post((req, res) => {
-        console.log(req.body)
         const newUser = new User({
             email: req.body.username,
             password: req.body.password
