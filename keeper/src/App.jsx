d197937814dd2210163f7d4ba2a919ff865ec118
Marian-N <mariannisky@gmail.com>
2022-06-13 09:58:52 +0200

    Keeper - add note component

--- keeper/src/App.jsx
+++ keeper/src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Note from "./components/note/Note";
 import "./styles.scss";
 
 const App = () => {
     return (
         <div id="App">
             <Header></Header>
+            <Note></Note>
             <Footer></Footer>
         </div>
     );
---
+++ keeper/src/components/note/Note.jsx
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+const Note = () => {
+    return ( 
+        <div class="note">
+            <h2>This is title</h2>
+            <p>This is content</p>
+        </div>
+     );
+}
+ 
+export default Note;
